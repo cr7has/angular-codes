2c14f4a5497eafed9adaeddde03eda5df3774d62
cr7 <rizwihasanakp@gmail.com>
2022-08-25 01:52:30 +0530

    just created toggler poc using directives

--- my-angular-projects/projects/amazon/src/app/Components/directives/directives.component.ts
+++ my-angular-projects/projects/amazon/src/app/Components/directives/directives.component.ts
@@ -5,4 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './directives.component.html',
   styleUrls: ['./directives.component.css'],
 })
-export class DirectivesComponent {}
+export class DirectivesComponent {
+  product = {
+    Name: 'Nike Casuals',
+    Price: 4000.0,
+    Photo: 'assets/shoes.jpg',
+  };
+  showImage = false;
+  btnText = 'Show ';
+  ToggleImage() {
+    this.showImage = this.showImage == false ? true : false;
+    this.btnText = this.btnText == 'Show ' ? 'Hide ' : 'Show ';
+  }
+}
