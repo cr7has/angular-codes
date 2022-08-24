9a8056015cd26bdd1fe43d4cfa89fc0e17181ff2
cr7 <rizwihasanakp@gmail.com>
2022-08-24 17:16:50 +0530

    these changes has been done by Angular cli commands like ng g c component componentName

--- my-angular-projects/projects/amazon/src/app/app.module.ts
+++ my-angular-projects/projects/amazon/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login.component';
 import { RegisterComponent } from './Components/register/register.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [
     //AppComponent
     //LoginComponent,
-    RegisterComponent,
+    //RegisterComponent,
+    ContactComponent,
   ],
 })
 export class AppModule {}
