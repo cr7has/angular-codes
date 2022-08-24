73cb9d59890245906f89d8a614b4fb6eac755d3e
cr7 <rizwihasanakp@gmail.com>
2022-08-25 01:23:41 +0530

    working with directives

---
+++ my-angular-projects/projects/amazon/src/app/Components/directives/directives.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css'],
+})
+export class DirectivesComponent {}
