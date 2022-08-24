895b19ab5ab03214f8a6057f024cd676241a03e2
cr7 <rizwihasanakp@gmail.com>
2022-08-24 17:31:47 +0530

    One way Data Binding using Interpolation

---
+++ my-angular-projects/projects/amazon/src/app/Components/databinding/databinding.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css'],
+})
+export class DatabindingComponent {
+  product = {
+    Name: 'Samsung TV',
+    Price: 45000.45,
+    InStock: true,
+  };
+}
